bba14437e51aa0ce074e334d63ad50fae4334bac
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-04-20 19:53:42 -0400

    Moving config to executable js files to take advantage of process

---
+++ src/config/development.js
@@ -0,0 +1,12 @@
+module.exports = {
+    redis: {
+        host: '127.0.0.1',
+        port: 6379,
+        database: 0
+    },
+    azure:{
+        account_name: 'manifolddev',
+        access_key:'y4nxuSBfRtukWKATRZR7Ji3zx+6hEtAGUwKxUQmuUY7q94lp1NqO453nNbiX/tYg7xnPUSojXMY8lQ5xJqClmw=='
+    },
+    platforms: ['android', 'ios', 'chrome', 'firefox']
+};
