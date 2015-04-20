b1f55e2f1f4f644ecf2b64c335499280e3f7f0f5
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-04-20 09:44:05 -0400

    Adding CORS headers

--- src/app.js
+++ src/app.js
@@ -25,6 +25,14 @@ var Manifold = {
 
         app.use('/manifests', manifests(redisClient));
 
+        app.use(function (req, res, next) {
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader('Access-Control-Allow-Credentials', true);
+            next();
+        });
+
         // catch 404 and forward to error handler
         app.use(function(req, res, next) {
             var err = new Error('Not Found');
