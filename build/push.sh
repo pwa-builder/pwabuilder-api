7969526ef542c29765e68cf595edb9598feda1ca
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-04-21 12:02:07 -0400

    Slight change to env vars

--- build/push.sh
+++ build/push.sh
@@ -3,6 +3,6 @@
 rm -rf .git
 git init
 git config user.name "BaM Interactive"
-git remote add azure "https://baminteractive:$STAGINGPASS@manifold-api-staging.scm.azurewebsites.net:443/manifold-api-staging.git" || true
+git remote add azure "https://baminteractive:$STAGING_PASS@manifold-api-staging.scm.azurewebsites.net:443/manifold-api-staging.git" || true
 git checkout -b deploy
 git push --force azure deploy:master
