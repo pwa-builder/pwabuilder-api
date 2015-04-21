a66a838489444764154e1ba68de1a77afb01b9ac
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-04-21 12:09:15 -0400

    Simplifying git steps for deployment

--- build/push.sh
+++ build/push.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-rm -rf .git
-git init
-git config user.name "BaM Interactive"
 git remote add azure "https://baminteractive:$STAGING_PASS@manifold-api-staging.scm.azurewebsites.net:443/manifold-api-staging.git" || true
 git checkout -b deploy
 git push --force azure deploy:master
