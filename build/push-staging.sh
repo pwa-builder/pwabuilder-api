72b79e26d83acfe2b149fdee4860b2c84b7a4947
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-05-11 15:40:54 -0400

    git is complaining about no username, this fixes that

--- build/push-staging.sh
+++ build/push-staging.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+git config user.email "interactive@bamideas.com"
+git config user.name "BaM Interactive"
 cp iisnode.staging.yml iisnode.yml
 git remote add azure "https://baminteractive:$STAGING_PASS@manifold-api-staging.scm.azurewebsites.net:443/manifold-api-staging.git" || true
 git checkout -b deploy
