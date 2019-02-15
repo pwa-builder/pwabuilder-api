794d0a13731436597cb17f69ec8d9240dbe0dcf6
Lucas Rojas <lucas.rojas@southworks.com>
2019-02-15 14:23:30 -0300

    Update view engine in app,js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ var PWABuilder = {
 
     // view engine setup
     app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'pug');
+    app.set('view engine', 'jade');
 
     app.use(logger('dev'));
 
