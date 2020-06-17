7c3a3334d280b8db9869db9e5cd30d884f2ce6ab
lee-leonardo <lee.leonardo.king@gmail.com>
2020-06-16 19:40:54 -0700

    get

--- src/routes/push.js
+++ src/routes/push.js
@@ -7,7 +7,7 @@ module.exports = function () {
   let service = new PushApi();
 
   return router
-    .post('/create', service.createVapidKey)
+    .get('/create', service.createVapidKey)
     .post('/register', service.registerVapidKey)
     .post('/unregister', service.unregisterVapidKey)
     .post('/subscribe', service.subscribeUser)
